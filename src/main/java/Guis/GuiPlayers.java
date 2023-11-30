9834e08d1cc32286ca9322fbb00486950d699991
javier-note <j.alcalde01@ufromail.cl>
2023-11-30 09:21:53 -0300

    comit

--- src/main/java/Guis/GuiPlayers.java
+++ src/main/java/Guis/GuiPlayers.java
@@ -4,4 +4,8 @@ import javax.swing.*;
 
 public class GuiPlayers {
     private JPanel Player;
+    private JTextArea textArea1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
 }
